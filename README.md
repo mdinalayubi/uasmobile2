795b9b73f1d98382e76b276ea10bf67faa20da89
mdinalayubi <mdinalayubi@gmail.com>
2024-07-02 10:21:49 +0700

    update

---
+++ README.md
@@ -0,0 +1,14 @@
+|  | |
+| ----------- | ----------- |
+| <b> Nama     | Muhammad Din Al Ayubi       |
+| <b> NIM     | 312210293       |
+| <b> Kelas   | TI.22.A.3        |
+| <b> Matakuliah   | UAS Pemrograman Mobile 2      |
+![img](mobile2/1.png)
+![img](mobile2/2.png)
+![img](mobile2/3.png)
+![img](mobile2/4.png)
+![img](mobile2/5.png)
+![img](mobile2/6.png)
+![img](mobile2/7.png)
+![img](mobile2/8.png)
---
+++ app/src/main/java/com/azhar/banksampah/viewmodel/InputDataViewModel.kt
@@ -0,0 +1,56 @@
+package com.azhar.banksampah.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.azhar.banksampah.database.DatabaseClient.Companion.getInstance
+import com.azhar.banksampah.database.dao.DatabaseDao
+import com.azhar.banksampah.model.ModelDatabase
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+/**
+ * Created by Azhar Rivaldi on 6-12-2021
+ * Youtube Channel : https://bit.ly/2PJMowZ
+ * Github : https://github.com/AzharRivaldi
+ * Twitter : https://twitter.com/azharrvldi_
+ * Instagram : https://www.instagram.com/azhardvls_
+ * LinkedIn : https://www.linkedin.com/in/azhar-rivaldi
+ */
+
+class InputDataViewModel(application: Application) : AndroidViewModel(application) {
+
+    var databaseDao: DatabaseDao?
+
+    fun addDataSampah(
+        nama_pengguna: String,
+        jenis_sampah: String,
+        berat: Int,
+        harga: Int,
+        tanggal: String,
+        alamat: String,
+        catatan: String
+    ) {
+        Completable.fromAction {
+            val modelDatabase = ModelDatabase(
+                namaPengguna = nama_pengguna,
+                jenisSampah = jenis_sampah,
+                berat = berat,
+                harga = harga,
+                tanggal = tanggal,
+                alamat = alamat,
+                catatan = catatan
+            )
+            databaseDao?.insertData(modelDatabase)
+        }
+
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
+    }
+
+    init {
+        databaseDao = getInstance(application)?.appDatabase?.databaseDao()
+    }
+
+}
